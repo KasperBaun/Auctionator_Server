d80dcb86893dee50053277477b88bedec60fddf6
Kasper Baun <kasperbaun@hotmail.com>
2022-01-10 11:52:57 +0100

    Server update - create auction init - testing on client atm

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,4 @@
-import org.jspace.SequentialSpace;
 import org.jspace.SpaceRepository;
-
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
@@ -12,9 +8,6 @@ public class Main {
 
         // Keep serving requests to enter or create auctions
         while (true) {
-
-            // auctionCount will be used to ensure every auction space has a unique name
-            Integer auctionCount = 0;
 
             server.listenForRequestToJoinAuction();
             server.listenForRequestToCreateAuction();
